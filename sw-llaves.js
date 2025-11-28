148af3d50f476c3c9320eaa62c18ae9bb1dc93a9
jeancarlozelaya <jz.92@live.com>
2025-11-28 08:59:47 -0600

    Update cache name and retain caching strategy

--- sw-llaves.js
+++ sw-llaves.js
@@ -1,6 +1,6 @@
 // Archivo: sw-llaves.js
 
-const CACHE_NAME_LLAVES = 'control-llaves-v1.0.0';
+const CACHE_NAME_LLAVES = 'control-llaves-v1.1.0';
 const urlsToCacheLlaves = [
     './Control de Llaves.html',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
@@ -77,3 +77,4 @@ self.addEventListener('fetch', function(event) {
             })
     );
 });
+

d8678ea6d951d71d361062b5b966a54eb622f82b
jeancarlozelaya <jz.92@live.com>
2025-12-18 09:47:35 -0600

    Update sw-index.js

--- sw-index.js
+++ sw-index.js
@@ -1,7 +1,7 @@
 // Archivo: sw-index.js
 
 // CAMBIAMOS LA VERSIÓN PARA QUE EL NAVEGADOR ACTUALICE LA CACHÉ SÍ O SÍ
-const CACHE_NAME_INDEX = 'pibrisa-index-v2.0.3'; 
+const CACHE_NAME_INDEX = 'pibrisa-index-v2.0.4'; 
 
 const urlsToCacheIndex = [
     './',

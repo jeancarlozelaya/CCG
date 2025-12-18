d28a550e97ca453222e6b9e5fcb0a34954e9599d
jeancarlozelaya <jz.92@live.com>
2025-12-18 09:41:26 -0600

    Update sw-index.js

--- sw-index.js
+++ sw-index.js
@@ -14,7 +14,9 @@ const urlsToCacheIndex = [
     './Registro de Residuos.html',
     './Registro de Trazabilidad & Volumen de Residuos.html',   
     'Imágenes/Icono.png',
-
+    
+    // LIBRERÍA DEXIE (¡IMPORTANTE PARA QUE NO FALLE OFFLINE!)
+    'https://unpkg.com/dexie/dist/dexie.js',
 
     // Librerías externas usadas en index.html
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',

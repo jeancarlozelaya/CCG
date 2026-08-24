534f69ab66dc744e3e45f379040231e3307ec880
jeancarlozelaya <jz.92@live.com>
2026-08-24 10:51:58 -0600

    Update cache version to v8.0.7

--- sw-index.js
+++ sw-index.js
@@ -1,7 +1,7 @@
 // Archivo: sw-index.js
 
 // CAMBIAMOS LA VERSIÓN PARA QUE EL NAVEGADOR ACTUALICE LA CACHÉ SÍ O SÍ
-const CACHE_NAME_INDEX = 'pibrisa-index-v8.0.6'; 
+const CACHE_NAME_INDEX = 'pibrisa-index-v8.0.7'; 
 
 const urlsToCacheIndex = [
     './',

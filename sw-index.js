f329e2d5c7f4eb8d08fbb8f394bd99a60fd77200
jeancarlozelaya <jz.92@live.com>
2026-09-17 15:05:38 -0600

    Update sw-index.js

--- sw-index.js
+++ sw-index.js
@@ -1,7 +1,7 @@
 // Archivo: sw-index.js
 
 // ⚠️ IMPORTANTE: Cambia esta versión CADA VEZ que hagas cambios
-const CACHE_NAME_INDEX = 'pibrisa-index-v8.3.82'; 
+const CACHE_NAME_INDEX = 'pibrisa-index-v8.3.83'; 
 
 const APP_VERSION = CACHE_NAME_INDEX.replace('pibrisa-index-', '');
 

fbbeb2f792047f7a30c48191f3b835f2f3747428
jeancarlozelaya <jz.92@live.com>
2026-09-17 19:59:01 -0600

    Update cache name version to 8.3.86

--- sw-index.js
+++ sw-index.js
@@ -1,7 +1,7 @@
 // Archivo: sw-index.js
 
 // ⚠️ IMPORTANTE: Cambia esta versión CADA VEZ que hagas cambios
-const CACHE_NAME_INDEX = 'pibrisa-index-v8.3.85'; 
+const CACHE_NAME_INDEX = 'pibrisa-index-v8.3.86'; 
 
 const APP_VERSION = CACHE_NAME_INDEX.replace('pibrisa-index-', '');
 
